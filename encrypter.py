b49ef93e6ae4a806e31d4a430adee7a786687662
SankirthGunnam <38679830+SankirthGunnam@users.noreply.github.com>
2026-01-23 16:26:25 +0530

    Refactor Encrypter to use static secret key
    Refactor Encrypter class to use a predefined secret key instead of retrieving it from the Windows registry.

--- encrypter.py
+++ encrypter.py
@@ -1,33 +1,13 @@
-import winreg
 from cryptography.fernet import Fernet
+secret_key = b"XC-O5ojdVONOnSEfvTSKq0Ib_KBFf6PCc0jAkYAgr0M="
 
 class Encrypter:
     def __init__(self):
-        self.registry_path = r"Software\BCFApp"
-        self.value_name = "SecretKey"
-        self.key = self._get_or_create_key()
+        self.key = secret_key
         self.cipher_suite = Fernet(self.key)
-
-    def _get_or_create_key(self):
-        try:
-            # Try to open the key
-            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, self.registry_path, 0, winreg.KEY_READ) as key:
-                value, _ = winreg.QueryValueEx(key, self.value_name)
-                # Fernet keys need to be bytes, registry strings are returned as strings if stored as REG_SZ
-                # But we store as bytes (REG_BINARY)
-                return value
-        except (FileNotFoundError, OSError):
-            # Key or value doesn't exist, create it
-            new_key = Fernet.generate_key()
-            with winreg.CreateKey(winreg.HKEY_CURRENT_USER, self.registry_path) as key:
-                winreg.SetValueEx(key, self.value_name, 0, winreg.REG_BINARY, new_key)
-            return new_key
 
     def encrypt(self, data):
         return self.cipher_suite.encrypt(data)
 
     def decrypt(self, data):
         return self.cipher_suite.decrypt(data)
-
-
-
